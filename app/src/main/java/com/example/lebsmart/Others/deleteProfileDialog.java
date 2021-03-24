d8f8f4b9f6dbd7bd013ee78dcffbd4a0ecf7f237
hadi-mubasher <72134354+hadi-mubasher@users.noreply.github.com>
2021-03-24 20:46:58 +0200

    Users can now delete their accounts.

--- app/src/main/java/com/example/lebsmart/Others/deleteProfileDialog.java
+++ app/src/main/java/com/example/lebsmart/Others/deleteProfileDialog.java
@@ -1,27 +1,15 @@
 package com.example.lebsmart.Others;
-
-
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
-
 import com.example.lebsmart.Activities.LoginActivity;
-import com.example.lebsmart.Activities.MainScreenActivity;
-import com.example.lebsmart.RandomFragments.CommonMethods;
-import com.example.lebsmart.RandomFragments.LoginTabFragment;
-import com.example.lebsmart.RandomFragments.MyProfileFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 public class deleteProfileDialog  extends DialogFragment {
     @Override
@@ -41,12 +29,9 @@ public class deleteProfileDialog  extends DialogFragment {
                         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                         user.delete();
                         Toast.makeText(getActivity(), "Your account was successfully deleted!", Toast.LENGTH_LONG).show();
-
                         Intent intent = new Intent(getActivity(), LoginActivity.class);
                         LoginActivity.sp.edit().putBoolean("loggedin", false).apply();
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        //finishAffinity();
-
                         startActivity(intent);
                     }
                 });
